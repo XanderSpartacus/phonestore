168eeb433e16ef41b993a7c87d7ec669078730e8
Yassine KHLOUFI <ykhloufi@jehann.fr>
2026-02-04 14:59:32 +0100

    Installation de FixturesBundle et création de UserFixtures pour ajouter 2 utilisateurs

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher
+    ){}
+
+    public function load(ObjectManager $manager): void
+    {
+        // USER ADMIN
+        $admin = new User();
+        $admin->setEmail('admin@phonestore.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordHasher->hashPassword($admin,'password'));
+
+        $manager->persist($admin);
+
+        // USER CLASSIQUE
+        $user = new User();
+        $user->setEmail('user@phonestore.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->passwordHasher->hashPassword($user,'password'));
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
