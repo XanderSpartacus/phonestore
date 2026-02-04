dd7930ecff76828ff8de9a6580a043f9a308a79e
Yassine KHLOUFI <ykhloufi@jehann.fr>
2026-02-04 11:54:48 +0100

    Afficher 3 produits sur la page produits avec la boucle for

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,6 +11,34 @@ class ProductController extends AbstractController
     #[Route('/produits', name: 'app_products')]
     public function index(): Response
     {
-        return $this->render('product/index.html.twig');
+        $products = [
+            [
+                'id' => 1,
+                'name' => 'iPhone 13 Pro',
+                'image' => 'iphone-13-pro.jpg',
+                'price' => 999.00,
+                'brand' => 'Apple',
+                'model' => 'iPhone 13'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Samsung Galaxy S21',
+                'image' => 'samsung-s21.jpg',
+                'price' => 799.00,
+                'brand' => 'Samsung',
+                'model' => 'Galaxy S21'
+            ],
+            [
+                'id' => 3,
+                'name' => 'Google Pixel 6a',
+                'image' => 'pixel-6a.jpg',
+                'price' => 459.00,
+                'brand' => 'Google',
+                'model' => 'Pixel 6a'
+            ]];
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products
+        ]);
     }
 }
